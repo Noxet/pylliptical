4eafc46dda4cfe71fcdf0a29795b4160318ddab8
Jonathan Sönnerup <Nooxet@gmail.com>
2019-02-28 16:13:06 +0100

    Added support for identity element

--- ecc.py
+++ ecc.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python3
 
+class ECPoint():
+    
+    def __init__(self, x, y, inf=0):
+        self.x = x
+        self.y = y
+        self.inf = inf
+
+    def __eq__(self, other):
+        if self.inf == other.inf:
+            return True
+
+        return (self.x == other.x) and (self.y == other.y)
+
+    def __repr__(self):
+        if self.inf == 1:
+            return 'O'
+        return '({}, {})'.format(self.x, self.y)
+
+
 class EllipticCurve():
 
-    def __init__(self, p, a, b):
+    def __init__(self, p, g, a, b):
+        """ A curve is defined by
+            p: The finite field Fp
+            g: The base point (generator) for the group
+            a, b: Curve parameters, Y^2 = X^3 + aX + b
+        """
         self.p = p
+        self.g = g
         self.a = a
         self.b = b
 
@@ -17,24 +42,35 @@ class EllipticCurve():
     def modinv(self, a, m):
         g, x, y = self.egcd(a, m)
         if g != 1:
-            raise Exception('modular inverse does not exist')
+            raise Exception('Modular inverse does not exist')
         else:
             return x % m
 
     def add(self, p1, p2):
         """ Adds two points P1 = (x1, y1) and P2 = (x2, y2) on the given curve. """
-        if p1[0] != p2[0]:
-            lam = ((p2[1] - p1[1]) * self.modinv((p2[0] - p1[0]) % self.p, self.p)) % self.p
+        # special case if one point is O (identity)
+        if p1.inf == 1 and p2.inf == 1:
+            return ECPoint(0, 0, 1)
+        if p1.inf == 1:
+            return p2
+        if p2.inf == 1:
+            return p1
+
+        if p1.x != p2.x:
+            lam = ((p2.y - p1.y) * self.modinv((p2.x - p1.x) % self.p, self.p)) % self.p
         else:
-            lam = (3*p1[0]**2 + self.a) * self.modinv(2 * p1[1], self.p)
+            if (p1.y == 0 or p2.y == 0) or (p2 == self.neg(p1)):
+                return ECPoint(0, 0, 1)
+
+            lam = ((3*p1.x**2 + self.a) * self.modinv(2 * p1.y, self.p)) % self.p
 
 
-        x3 = (lam**2 - p1[0] - p2[0]) % self.p
-        y3 = ((p1[0] - x3) * lam - p1[1]) % self.p
-        return (x3, y3)
+        x3 = (lam**2 - p1.x - p2.x) % self.p
+        y3 = ((p1.x - x3) * lam - p1.y) % self.p
+        return ECPoint(x3, y3)
 
     def neg(self, p):
-        return (p[0], self.p - p[1])
+        return ECPoint(p.x, self.p - p.y)
 
     def sub(self, p1, p2):
         """ Subtract P2 from P1, i.e., P1 - P2 = P1 + (-P2). """
