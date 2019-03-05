009c878ff55e0911a8d101b0e01bbb9454661424
Jonathan Sönnerup <Nooxet@gmail.com>
2019-03-05 15:56:43 +0100

    Implemented a hashed version of elgamal to avoid mapping plaintext to a curve point.

--- elgamal.py
+++ elgamal.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+Implementation of ElGamal over elliptic curves.
+This version uses hash-based approach to encrypt a message M,
+in order to avoid mapping M to a curve point.
+
+For injective encodings to elliptic curves, see https://eprint.iacr.org/2013/373.pdf
+
+Noxet
+2019
+"""
+
 from random import randint
+from hashlib import sha256
+
 from ecc import EllipticCurve, ECPoint
 
 def keygen():
@@ -32,11 +45,15 @@ def encrypt(m, G, g, p, h):
     h: Public part of the shared secret
     """
 
+    # TODO: implement a padding scheme for m, like OAEP
+
     y = randint(1, p)
     c1 = G.mul(g, y)
     s = G.mul(h, y)         # h*y = g*xy
-    c2 = G.add(m, s)
-    return (c1, c2)
+    # c2 = m xor H(h*y)
+    hs = sha256(repr(s).encode('utf-8')).digest()
+    c2 = bytearray([i ^ j for i,j in zip(m, hs)])
+    return (c1, bytes(c2))
 
 
 def decrypt(c, x, G):
@@ -52,16 +69,18 @@ def decrypt(c, x, G):
         
     c1, c2 = c
     s = G.mul(c1, x)
-    m = G.sub(c2, s)
-    return m
+    # m = c2 xor H(c1*x)
+    hs = sha256(repr(s).encode('utf-8')).digest()
+    m = bytearray([i ^ j for i,j in zip(c2, hs)])
+    return bytes(m)
 
 
 if __name__ == '__main__':
     x, G, g, p, h = keygen()
-    m = G.random_point()
-    print('Message:\t{}'.format(m))
+    m = input('Enter message: ').encode('utf-8')
 
     c = encrypt(m, G, g, p, h)
+    print('Encrypted: {}'.format(c))
     mp = decrypt(c, x, G)
-    print('Decrypted:\t{}'.format(mp))
+    print('Decrypted:\t{}'.format(mp.decode()))
     assert m == mp
